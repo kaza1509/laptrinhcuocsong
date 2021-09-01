8fe3718a7ce085ce11c2d517dc0f62fd7f6a6524
kaza1509 <kazafpt@gmail.com>
2021-09-02 01:29:27 +0700

    laptrinhcuocsong

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var x = document.getElementsByTagName('a');
+for(e of x) {
+    e.setAttribute
+    ('href','https://www.facebook.com/kaza15092001/');
+    e.setAttribute('title','Trang này của Nguyễn Tiền Khôi')
+    e.addEventListener('click',() => {
+        alert('you are so handsome girl/boy');
+    })
+    e.setAttribute('target','_blank')
+}
+
+
