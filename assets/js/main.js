a4ec36b6b43986058f5847f9500d49f73b90e48f
Khoi Nguyen <kazafpt@gmail.com>
2021-12-25 02:42:09 +0700

    responsive nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,32 @@
 var x = document.getElementsByTagName('a');
 for(e of x) {
-    e.setAttribute
-    ('href','https://www.facebook.com/kaza15092001/');
     e.setAttribute('title','Trang này của Nguyễn Tiền Khôi')
-    e.addEventListener('click',() => {
-        alert('you are so handsome girl/boy');
-    })
-    e.setAttribute('target','_blank')
 }
 
+let bar = document.getElementById('bar')
+let close = document.getElementById('close')
+let header = document.getElementById('header')
+let over = document.getElementById('overlay')
+let logo = document.querySelector('header h1')
 
+bar.addEventListener('click', (e) => {
+    header.style.transform = 'translateX(0%)'
+    header.style.transition = 'all .4s linear'
+    over.style.display = 'block'
+    logo.style.display = 'none'
+})
+
+close.addEventListener('click', (e) => {
+    header.style.transform = 'translateX(-100%)'
+    header.style.transition = 'all .4s linear'
+    over.style.display = 'none'
+    logo.style.display = 'block'
+
+})
+
+over.addEventListener('click', (e) => {
+    header.style.transform = 'translateX(-100%)'
+    header.style.transition = 'all .4s linear'
+    over.style.display = 'none'
+    logo.style.display = 'block'
+})
